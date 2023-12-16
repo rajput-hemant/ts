5cebc83b6ac1e2fbb90288e9890a77c34225e61e
rajput-hemant <rajput.hemant2001@gmail.com>
2023-12-17 00:30:23 +0530

    feat: solved day 5 & 6 type challenges
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ advent-of-typescript/2023/day_06.ts
@@ -0,0 +1,35 @@
+import { Equal, Expect } from "type-testing";
+
+/* -----------------------------------------------------------------------------------------------
+ * Code here
+ * -----------------------------------------------------------------------------------------------*/
+
+type FilterChildrenBy<T, U> = Exclude<T, U>;
+
+/* -----------------------------------------------------------------------------------------------
+ * Do not edit below this line
+ * -----------------------------------------------------------------------------------------------*/
+
+type test_0_actual = FilterChildrenBy<
+  //   ^?
+  "nice" | "nice" | "nice",
+  "naughty"
+>;
+type test_0_expected = "nice";
+type test_0 = Expect<Equal<test_0_expected, test_0_actual>>;
+
+type test_1_actual = FilterChildrenBy<
+  //   ^?
+  "nice" | "naughty" | "naughty",
+  "naughty"
+>;
+type test_1_expected = "nice";
+type test_1 = Expect<Equal<test_1_expected, test_1_actual>>;
+
+type test_2_actual = FilterChildrenBy<
+  //   ^?
+  string | number | (() => void),
+  Function
+>;
+type test_2_expected = string | number;
+type test_2 = Expect<Equal<test_2_expected, test_2_actual>>;
